a67004860ceab8bf1f35d22590a35724a3e12f20
AMM <ansmartin@users.noreply.github.com>
2025-06-22 15:50:15 +0200

    añadidas imágenes de los tipos en la página de Minigame

--- pages/2_👾_Minigame.py
+++ pages/2_👾_Minigame.py
@@ -42,57 +42,62 @@ with col1:
 
 
     # types
-    typing = f'{poke.first_type.upper()}'
-    if(poke.second_type):
-        typing += f' / {poke.second_type.upper()}' 
+    minicol1, minicol2, minicol3, minicol4  = st.columns([1,1,1,1])
+    with minicol1:
+        st.markdown(f'#### :grey[HINT 1.] Type:')
+    with minicol2:
+        if(poke.first_type):
+            st.image(f'./images/types/{poke.first_type}.png', use_container_width=True)
+    with minicol3:
+        if(poke.second_type):
+            st.image(f'./images/types/{poke.second_type}.png', use_container_width=True)
 
-    st.markdown(f'#### :grey[HINT 1:] Type {typing}')
 
-    #st.markdown(f'#### :grey[HINT 2:] Species color {poke.color_name.upper()}')
+    #st.markdown(f'#### :grey[HINT 2.] Species color {poke.color_name.upper()}')
 
-    st.markdown(f'#### :grey[HINT 2:] Name starts with the letter {poke.pokemon_name[0].upper()}')
+    st.markdown(f'#### :grey[HINT 2.] Name starts with the letter {poke.pokemon_name[0].upper()}')
 
-    hint4 = f'#### :grey[HINT 3:] This Pokémon was introduced in generation {poke.pokemon_generation_number}'
+    hint4 = f'#### :grey[HINT 3.] This Pokémon was introduced in generation {poke.pokemon_generation_number}'
     if not poke.is_default and poke.pokemon_generation_number>poke.species_generation_number:  
         hint4 += f', but the Pokémon species was introduced in generation {poke.species_generation_number}'
     
     st.markdown(hint4)
 
-    st.markdown(f'#### :grey[HINT 4:] Has pre-evolution {get_tick_emoji(poke.evolves_from_pokemon_base_name!=None)}')
-    st.markdown(f'#### :grey[HINT 5:] Has evolution {get_tick_emoji(poke.evolutions.size>0)}')
+    st.markdown(f'#### :grey[HINT 4.] Has pre-evolution: {get_tick_emoji(poke.evolves_from_pokemon_base_name!=None)}')
+    st.markdown(f'#### :grey[HINT 5.] Has evolution: {get_tick_emoji(poke.evolutions.size>0)}')
 
 
     '\n'
     '\n'
 
     if poke.is_default:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] The color of this Pokémon is {poke.color_name.upper()}')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] The color of this Pokémon is {poke.color_name.upper()}')
 
         if poke.has_gender_differences:
-            st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon has gender differences')
+            st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon has gender differences')
 
     if poke.is_baby:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is considered baby')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is considered baby')
     if poke.is_legendary:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is considered legendary')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is considered legendary')
     if poke.is_mythical:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is considered mythical')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is considered mythical')
 
     if poke.is_mega:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is Mega-Evolved')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is Mega-Evolved')
     elif poke.is_gmax:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is Gigantamax')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is Gigantamax')
     elif poke.is_regional:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is a regional form')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is a regional form')
     elif not poke.is_default:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon is not considered the default within its own species')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon is not considered the default within its own species')
 
     if poke.has_mega:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon can Mega-Evolve')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon can Mega-Evolve')
     if poke.has_gmax:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon can Gigantamax')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon can Gigantamax')
     if poke.has_regional:
-        st.markdown(f'#### :grey[ADDITIONAL HINT:] This Pokémon has a regional form')
+        st.markdown(f'#### :grey[ADDITIONAL HINT.] This Pokémon has a regional form')
 
 
 with col2:
