ee92614d4d8cb406a087c8044649e1e37f3a38cb
AMM <ansmartin@users.noreply.github.com>
2025-06-26 01:45:12 +0200

    modificada página de Filters con más opciones

--- pages/1_📝_Filters.py
+++ pages/1_📝_Filters.py
@@ -37,6 +37,17 @@ colors_list = [
     'green', 'red', 'blue', 'white', 'brown', 
     'yellow', 'purple', 'pink', 'gray', 'black'
 ]
+shape_list = [
+    'quadruped', 'upright', 'armor', 'squiggle', 'bug-wings', 'wings',
+    'humanoid', 'legs', 'blob', 'heads', 'tentacles', 'arms', 'fish', 'ball'
+]
+egg_group_list = [
+    'ground','dragon','flying','mineral',
+    'water1','water2','water3',
+    'plant','bug','fairy',
+    'ditto', 'monster','humanshape','indeterminate',
+    'no-eggs', None
+ ]
 gens_list = [ 
     x for x in range(1,10) 
 ]
@@ -61,72 +72,6 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     with st.container(border=True):
-        
-        checked = st.checkbox('Filter by type')
-
-        if checked:
-            options = ['Specify type position', 'Any type position'] #'Don\'t filter by type'
-
-            option = st.selectbox(
-                #'Filter by type:',
-                '',
-                options
-            )
-
-            #types_list.append(None)
-
-            with st.container(border=True):
-                if option==options[0]:
-                    with st.container():
-                        first_types_selected = st.multiselect("First type", types_list)
-                    with st.container():
-                        second_types_selected = st.multiselect("Second type", types_list)
-
-                    if len(first_types_selected)==0:
-                        df = df[df.second_type.isin(second_types_selected)]
-                    elif len(second_types_selected)==0:
-                        df = df[df.first_type.isin(first_types_selected)]
-                    else:
-                        df = df[(df.first_type.isin(first_types_selected)) & (df.second_type.isin(second_types_selected))]
-
-                elif option==options[1]:
-                    types_container = st.container()
-                    with types_container:
-                        types_selected = st.multiselect("Type", types_list)
-                        df = df[(df.first_type.isin(types_selected)) | (df.second_type.isin(types_selected))]
-
-    with st.container(border=True):
-        gens_selected = st.multiselect("Pokémon species generation", gens_list)
-        if len(gens_selected)>0:
-            df = df[df.species_generation_number.isin(gens_selected)]
-            
-        gens_selected = st.multiselect("Pokémon form generation", gens_list)
-        if len(gens_selected)>0:
-            df = df[df.form_generation_number.isin(gens_selected)]
-            
-    with st.container(border=True):
-        games_selected = st.multiselect("Game version", games_list)
-        if len(games_selected)>0:
-            df = df[df.version_group_name.isin(games_selected)]
-
-with col2:
-    with st.container(border=True):
-        # forms = ['Default', 'Mega', 'GMax', 'Regional']
-
-        # with st.container():
-        #     forms_selected = st.multiselect("Forms", forms, default=forms)
-
-        # for x in forms_selected:
-        #     if x==forms[0]:
-        #         df = df[df.is_default]
-        #     elif x==forms[1]:
-        #         df = df[df.is_mega]
-        #     elif x==forms[2]:
-        #         df = df[df.is_gmax]
-        #     elif x==forms[3]:
-        #         df = df[df.is_regional]
-                
-
         'Include forms:'
         include_default = st.checkbox("Default species", value=True)
         include_regional = st.checkbox("Regional forms", value=True)
@@ -150,15 +95,166 @@ with col2:
         baby = st.checkbox("Baby", value=False)
         legendary = st.checkbox("Legendary", value=False)
         mythical = st.checkbox("Mythical", value=False)
+        totem = st.checkbox("Totem", value=False)
+        #battle = st.checkbox("Battle-only forms", value=False)
+        
         if baby:
             df = df[df.is_baby]
         if legendary:
             df = df[df.is_legendary]
         if mythical:
             df = df[df.is_mythical]
+        if totem:
+            df = df[df.is_totem]
+        # if battle:
+        #     df = df[df.is_battle_only]
+
+
+with col2:
+    with st.container(border=True):
+        
+        checked = st.checkbox('Filter by type')
+
+        if checked:
+            options = ['Any type position', 'Specify type position']
+
+            option = st.selectbox(
+                #'Filter by type:',
+                '',
+                options
+            )
+
+            #types_list.append(None)
+
+            with st.container(border=True):
+                if option==options[1]:
+                    with st.container():
+                        first_types_selected = st.multiselect("First type", types_list)
+                    with st.container():
+                        second_types_selected = st.multiselect("Second type", types_list)
+                
+                    if len(first_types_selected)==0 and len(second_types_selected)==0:
+                        pass
+                    else:
+                        cond1 = cond2 = True
+                        if len(first_types_selected)>0:
+                            cond1 = (df.first_type.isin(first_types_selected))
+                        if len(second_types_selected)>0:
+                            cond2 = (df.second_type.isin(second_types_selected))
+
+                        df = df[(cond1) & (cond2)]
+
+                elif option==options[0]:
+                    types_container = st.container()
+
+                    with types_container:
+                        types_selected = st.multiselect("Type", types_list)
+
+                        df = df[(df.first_type.isin(types_selected)) | (df.second_type.isin(types_selected))]
+
+    with st.container(border=True):
+
+        checked = st.checkbox('Filter by ability')
+
+        if checked:
+            options = ['Any ability position', 'Specify ability position']
+
+            option = st.selectbox(
+                '',
+                options
+            )
+
+            abilities = set(df.first_ability)
+            abilities.update(set(df.second_ability))
+            abilities.update(set(df.hidden_ability))
+            abilities_list = list(abilities)
+
+            with st.container(border=True):
+                if option==options[1]:
+                    with st.container():
+                        first_abilities_selected = st.multiselect("First ability", abilities_list)
+                    with st.container():
+                        second_abilities_selected = st.multiselect("Second ability", abilities_list)
+                    with st.container():
+                        hidden_abilities_selected = st.multiselect("Hidden ability", abilities_list)
+
+                    if len(first_abilities_selected)==0 and len(second_abilities_selected)==0 and len(hidden_abilities_selected)==0:
+                        pass
+                    else:
+                        cond1 = cond2 = cond3 = True
+                        if len(first_abilities_selected)>0:
+                            cond1 = (df.first_ability.isin(first_abilities_selected))
+                        if len(second_abilities_selected)>0:
+                            cond2 = (df.second_ability.isin(second_abilities_selected))
+                        if len(hidden_abilities_selected)>0:
+                            cond3 = (df.hidden_ability.isin(hidden_abilities_selected))
+
+                        df = df[(cond1) & (cond2) & (cond3)]
+
+                elif option==options[0]:
+                    abilities_container = st.container()
+
+                    with abilities_container:
+                        abilities_selected = st.multiselect("Ability", abilities_list)
+
+                        df = df[
+                            (df.first_ability.isin(abilities_selected)) | 
+                            (df.second_ability.isin(abilities_selected)) | 
+                            (df.hidden_ability.isin(abilities_selected))
+                        ]
+
+    with st.container(border=True):
+        gens_selected = st.multiselect("Filter by Pokémon species generation", gens_list)
+        if len(gens_selected)>0:
+            df = df[df.species_generation_number.isin(gens_selected)]
+            
+        gens_selected = st.multiselect("Filter by Pokémon form generation", gens_list)
+        if len(gens_selected)>0:
+            df = df[df.form_generation_number.isin(gens_selected)]
+            
+    with st.container(border=True):
+        games_selected = st.multiselect("Filter by game version", games_list)
+        if len(games_selected)>0:
+            df = df[df.version_group_name.isin(games_selected)]
 
 
 with col3:
+    with st.container(border=True):
+        
+        checked = st.checkbox('Filter by egg group')
+
+        if checked:
+            options = ['Any egg group position', 'Specify egg group position']
+
+            option = st.selectbox(
+                '',
+                options
+            )
+
+            with st.container(border=True):
+                if option==options[1]:
+                    with st.container():
+                        first_eg_selected = st.multiselect("First egg group", egg_group_list)
+                    with st.container():
+                        second_eg_selected = st.multiselect("Second egg group", egg_group_list)
+
+                    if len(first_eg_selected)==0 and len(second_eg_selected)==0:
+                        pass
+                    else:
+                        cond1 = cond2 = True
+                        if len(first_eg_selected)>0:
+                            cond1 = (df.first_egg_group.isin(first_eg_selected))
+                        if len(second_eg_selected)>0:
+                            cond2 = (df.second_egg_group.isin(second_eg_selected))
+
+                        df = df[(cond1) & (cond2)]
+
+                elif option==options[0]:
+                    eggs_container = st.container()
+                    with eggs_container:
+                        eggs_selected = st.multiselect("Egg group", egg_group_list)
+                        df = df[(df.first_egg_group.isin(eggs_selected)) | (df.second_egg_group.isin(eggs_selected))]
+
     with st.container(border=True):
         cond1 = cond2 = True
         checked_h = st.checkbox('Filter by height')
@@ -179,33 +275,60 @@ with col3:
             df = df[cond2]
 
     with st.container(border=True):
-        colors_selected = st.multiselect("Color", colors_list)
+        shapes_selected = st.multiselect("Filter by shape", shape_list)
+        if len(shapes_selected)>0:
+            df = df[df.shape_name.isin(shapes_selected)]
+
+        colors_selected = st.multiselect("Filter by color", colors_list)
         if len(colors_selected)>0:
             df = df[df.color_name.isin(colors_selected)]
 
+    with st.container(border=True):
+        options = ['None','Select Pokémon without pre-evolution', 'Select Pokémon with pre-evolution']
+
+        option = st.selectbox(
+            'Filter by pre-evolution',
+            options
+        )
+
+        if option==options[0]:
+            pass
+        elif option==options[1]:
+            df = df[df.evolves_from_pokemon_base_name.isna()]
+        else:
+            df = df[df.evolves_from_pokemon_base_name.notna()]
+
+    with st.container(border=True):
+        checked = st.checkbox('Filter by evolutions')
+        if checked:
+            total_slider = st.slider('Number of potential evolutions', 0, 8, (0,8))
+            condition = (df.evolutions.apply(lambda values: len(values)).between(total_slider[0], total_slider[1]))
+            df = df[condition]
+
+         
 with col4:
     with st.container(border=True):
         checked = st.checkbox('Filter by base stats')
 
         if checked:
-            hp_slider = st.slider('HP', 0, 255, (0,255))
-            attack_slider = st.slider('Attack', 0, 255, (0,255))
-            defense_slider = st.slider('Defense', 0, 255, (0,255))
-            sp_attack_slider = st.slider('SpecialAttack', 0, 255, (0,255))
-            sp_defense_slider = st.slider('SpecialDefense', 0, 255, (0,255))
-            speed_slider = st.slider('Speed', 0, 255, (0,255))
+            with st.container(border=True):
+                hp_slider = st.slider('HP', 0, 255, (0,255))
+                attack_slider = st.slider('Attack', 0, 255, (0,255))
+                defense_slider = st.slider('Defense', 0, 255, (0,255))
+                sp_attack_slider = st.slider('SpecialAttack', 0, 255, (0,255))
+                sp_defense_slider = st.slider('SpecialDefense', 0, 255, (0,255))
+                speed_slider = st.slider('Speed', 0, 255, (0,255))
 
-            condition = (
-                (df.stat_hp_base.between(hp_slider[0], hp_slider[1])) &
-                (df.stat_attack_base.between(attack_slider[0], attack_slider[1])) &
-                (df.stat_defense_base.between(defense_slider[0], defense_slider[1])) &
-                (df.stat_special_attack_base.between(sp_attack_slider[0], sp_attack_slider[1])) &
-                (df.stat_special_defense_base.between(sp_defense_slider[0], sp_defense_slider[1])) &
-                (df.stat_speed_base.between(speed_slider[0], speed_slider[1]))
-            )
-            df = df[condition]
+                condition = (
+                    (df.stat_hp_base.between(hp_slider[0], hp_slider[1])) &
+                    (df.stat_attack_base.between(attack_slider[0], attack_slider[1])) &
+                    (df.stat_defense_base.between(defense_slider[0], defense_slider[1])) &
+                    (df.stat_special_attack_base.between(sp_attack_slider[0], sp_attack_slider[1])) &
+                    (df.stat_special_defense_base.between(sp_defense_slider[0], sp_defense_slider[1])) &
+                    (df.stat_speed_base.between(speed_slider[0], speed_slider[1]))
+                )
+                df = df[condition]
 
-    with st.container(border=True):
         checked = st.checkbox('Filter by total base stats')
 
         if checked:
@@ -213,10 +336,38 @@ with col4:
             condition = (df.stats_total.between(total_slider[0], total_slider[1]))
             df = df[condition]
 
-#df.first_type = df.first_type.apply(lambda x: f'./images/types/{x}.png')
-#df.second_type = df.second_type.apply(lambda x: f'./images/types/{x}.png')
+    with st.container(border=True):
+        checked = st.checkbox('Filter by effort values')
+
+        if checked:
+            with st.container(border=True):
+                hp_slider = st.slider('HP', 0, 3, (0,3))
+                attack_slider = st.slider('Attack', 0, 3, (0,3))
+                defense_slider = st.slider('Defense', 0, 3, (0,3))
+                sp_attack_slider = st.slider('SpecialAttack', 0, 3, (0,3))
+                sp_defense_slider = st.slider('SpecialDefense', 0, 3, (0,3))
+                speed_slider = st.slider('Speed', 0, 3, (0,3))
+
+                condition = (
+                    (df.stat_hp_effort.between(hp_slider[0], hp_slider[1])) &
+                    (df.stat_attack_effort.between(attack_slider[0], attack_slider[1])) &
+                    (df.stat_defense_effort.between(defense_slider[0], defense_slider[1])) &
+                    (df.stat_special_attack_effort.between(sp_attack_slider[0], sp_attack_slider[1])) &
+                    (df.stat_special_defense_effort.between(sp_defense_slider[0], sp_defense_slider[1])) &
+                    (df.stat_speed_effort.between(speed_slider[0], speed_slider[1]))
+                )
+                df = df[condition]
+
+        checked = st.checkbox('Filter by total effort values')
+
+        if checked:
+            total_slider = st.slider('Total', 0, 4, (0,4))
+            condition = (df.effort_total.between(total_slider[0], total_slider[1]))
+            df = df[condition]
+
 
 '---'
+
 st.markdown(f'#### Data')
 
 f'Number of results: {df.shape[0]} rows'
@@ -224,9 +375,5 @@ f'Number of results: {df.shape[0]} rows'
 
 st.dataframe(
     df, 
-    # column_config={
-    #     'first_type' : st.column_config.ImageColumn(),
-    #     'second_type' : st.column_config.ImageColumn(),
-    # },
     hide_index=True
 )
